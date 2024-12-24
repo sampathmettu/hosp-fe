beba81fefcfa52292d3e72be947a4d1cc898faa7
sampathmettu <sampathmettu@gmail.com>
2024-12-24 17:27:35 +0530

    Update master.service.ts
    ec2-13-201-26-109.ap-south-1.compute.amazonaws.com

--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class MasterService {
   
   production:true | undefined
-  apiEndPoint: string = "http://ec2-43-205-255-47.ap-south-1.compute.amazonaws.com:8080/api/HospitalAppointment/";
+  apiEndPoint: string = "http://ec2-13-201-26-109.ap-south-1.compute.amazonaws.com:8080/api/HospitalAppointment/";
   constructor(private http: HttpClient) { }
 
   createNew(obj:any): Observable<any> {
